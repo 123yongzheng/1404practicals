7305d01fa630470e93053f71e6e559bea4efc40f
123yongzheng <yongzheng.chen@my.jcu.edu.au>
2023-11-20 14:10:18 +0800

    score

---
+++ prac_02/score.py
@@ -0,0 +1,17 @@
+MAX_SCORE = 100
+EXCELLENT_SCORE = 90
+PASS_SCORE = 50
+
+
+def get_score_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+use_score = float(input("Enter score: "))
+result = get_score_result(use_score)
+print(result)
